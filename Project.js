3c2a5e791a070b2eec5dea87972155aff7ccd10e
Rechiel Mae Inderio <r.inderio.533722@umindanao.edu.ph>
2026-03-12 14:19:11 +0800

    second commit

---
+++ Project.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+<main>input this</main>
+    class Project {
+        constructor(name, description, startDate, endDate) {
+        this.name = name;   
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+  }     
+}
